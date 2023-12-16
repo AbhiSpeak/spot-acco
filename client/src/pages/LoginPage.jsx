278653d919829255c4f9b2c65872534dfd3c17aa
AbhiSpeak <abhi0108jha@gmail.com>
2023-12-16 12:22:21 +0530

    created different pages for the webapp.

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function LoginPage () {
+    return (
+        <div>Login Page baybay</div>
+    )
+}
+
+export default LoginPage
---
+++ client/src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MainPage () {
+    return (
+        <div>
+            main page baybay
+        </div>
+    )
+}
+
+export default MainPage
