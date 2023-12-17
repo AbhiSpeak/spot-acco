eba889f31727868b78dfd8404b2d453040c9c543
AbhiSpeak <abhi0108jha@gmail.com>
2023-12-17 14:18:29 +0530

    modified login page.

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,51 @@
-import React from "react";
-
+import axios from "axios";
+import React, {useState} from "react";
+import { Link, Navigate } from "react-router-dom";
 function LoginPage () {
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+    const[redirect, setRedirect] = useState(false)
+
+    async function loginUser (e) {
+        e.preventDefault()
+        try {
+            await axios.post('/login', {
+                email,
+                password
+            })
+            alert("Logged in successfully")
+            setRedirect(true)
+        } catch (e) {
+            alert('Login failed')
+        }
+    }
+
+    if(redirect)
+        return <Navigate to={'/'} />
     return (
-        <div>Login Page baybay</div>
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mb-64">
+                <h1 className="text-4xl text-center mb-4">Login</h1>
+                <form className="max-w-md mx-auto" onSubmit={loginUser}>
+                    <input 
+                        type="email" 
+                        placeholder="yourmail@email.com"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <button className="loginBtn">Login</button>
+
+                    <div className="text-center py-2 text-gray-500">Don't have an account yet? <Link className="underline text-black" to={'/register'}>Register</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
