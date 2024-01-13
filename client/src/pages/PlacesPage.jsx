48e03d40ed6af009de822a91961c2d267d0fc4d8
AbhiSpeak <abhi0108jha@gmail.com>
2024-01-13 12:13:52 +0530

    list of places will be displayed.

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import AccountNav from "../components/AccountNav";
+import axios from 'axios'
 
 function PlacesPage () {
-    
+    const [places, setPlaces] = useState([])
+    useEffect(() => {
+        axios.get('/places').then(({data}) => {
+            setPlaces(data)
+        })
+    }, [])
     return (
         <div>
             <AccountNav />
-            ayooooo
                 <div className="text-center">
                     <Link className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full" to={'/account/places/new'}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -15,6 +20,21 @@ function PlacesPage () {
                         </svg>
                         Add new place
                     </Link>
+                </div>
+                <div className="mt-4">
+                    {places.length > 0 && places.map(place => (
+                        <Link to={'/account/places/' + place._id} className="flex cursor-pointer gap-4 bg-gray-200 p-4 rounded-2xl ">
+                            <div className="w-32 h-32 bg-gray-300 flex">
+                            {place.addedPhotos.length > 0 && (
+                                <img className="object-cover" src={'http://localhost:4040/uploads/'+place.addedPhotos[1]} alt="place image" />
+                            )}
+                            </div>
+                            <div className="grow-0">
+                            <h2 className="text-xl">{place.title}</h2>
+                            <p className="text-sm mt-2">{place.description}</p>
+                            </div>
+                        </Link>
+                    ))}
                 </div>
         </div>
     )
