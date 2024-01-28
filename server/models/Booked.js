7a32f17fcc17f4e66810f9355d7fc8193bea0c55
AbhiSpeak <abhi0108jha@gmail.com>
2024-01-28 16:24:16 +0530

    new model for saving the booked record.

---
+++ server/models/Booked.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const bookedSchema = new mongoose.Schema({
+    place: {
+        type:mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    checkIn: {
+        type: Date,
+        required: true
+    },
+    checkOut: {
+        type: Date,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+    }
+})
+
+const BookedModel = mongoose.model('Booking', bookedSchema)
+
+module.exports = BookedModel
