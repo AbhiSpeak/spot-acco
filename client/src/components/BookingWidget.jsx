66a9e59ee487673cb82c937aeeaa8c543a24cf55
AbhiSpeak <abhi0108jha@gmail.com>
2024-01-28 16:23:20 +0530

    implemented the booking feature.

--- client/src/components/BookingWidget.jsx
+++ client/src/components/BookingWidget.jsx
@@ -1,25 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import {differenceInCalendarDays} from 'date-fns'
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 function BookingWidget ({place}) {
+    const [checkIn, setCheckIn] = useState('')
+    const [checkOut, setCheckOut] = useState('')
+    const [guest, setGuest] = useState(1)
+
+    const [name, setName] = useState('')
+    const [mobileNo, setMobileNo] = useState('')
+    const [redirect, setRedirect] = useState('')
+
+    let numberOfNights = 0;
+    if(checkOut && checkIn ) {
+        numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn))
+    }
+
+    async function handleBooking () {
+        const data = {
+            place: place._id, checkIn, checkOut, guest, name, mobile: mobileNo, price: numberOfNights*place.price}
+        const res = await axios.post('/booking', data)
+
+        const bookedId = res.data._id
+        setRedirect(`/account/bookings/${bookedId}`)
+    }
+
+    if(redirect) {
+        return <Navigate to={redirect} />
+    }
     return (
         <div className="bg-white shadow p-4 rounded-2xl">
                         <h2 className="text-2xl text-center">Price: ${place.price} / per night</h2>
                         <div className="flex">
                         <div className="my-4 border py-4 px-4 rounded-2xl">
                             <label>Check in: </label>
-                            <input type='date' />
+                            <input 
+                                type='date' 
+                                value={checkIn} 
+                                onChange={e => setCheckIn(e.target.value)} 
+                            />
                         </div>
                         <div className="my-4 border border-l py-4 px-4 rounded-2xl">
                             <label>Check out: </label>
-                            <input type='date' />
+                            <input 
+                                type='date' 
+                                value={checkOut} 
+                                onChange={e => setCheckOut(e.target.value)} 
+                            />
                         </div>
                         
                         </div>
                         <div className="my-4 border border-l py-4 px-4 rounded-2xl">
                             <label>Number of guests: </label>
-                            <input type='number' value={1} />
+                            <input
+                                type='number' 
+                                value={guest} 
+                                onChange={e => setGuest(e.target.value)} 
+                            />
                         </div>
-                        <button className="loginBtn mt-4">Book this place</button>
+                        {numberOfNights > 0 && (
+                            <>
+                            <div className="my-4 border border-l py-4 px-4 rounded-2xl">
+                                <label>Your Name: </label>
+                                <input
+                                    type='text' 
+                                    value={name} 
+                                    onChange={e => setName(e.target.value)} 
+                                />
+                            </div>
+                            <div className="my-4 border border-l py-4 px-4 rounded-2xl">
+                            <label>Your Mobile No: </label>
+                            <input
+                                type='tel' 
+                                value={mobileNo} 
+                                onChange={e => setMobileNo(e.target.value)} 
+                            />
+                        </div>
+                        </>
+                        )}
+                        <button onClick={handleBooking} className="loginBtn mt-4">
+                            Book this place
+                            {numberOfNights > 0 && (
+                                <span> (${numberOfNights * place.price})</span>
+                            )}
+                            
+                            </button>
                     </div>
     )
 }
